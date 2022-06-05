5fd4f2dc605a4ecdb4984e14833cafda981003c3
JasGod <58049174+JasGod@users.noreply.github.com>
2022-06-05 09:48:25 +0100

    Update cert_code.py
    Convert to Json.

--- api_python/cert_code.py
+++ api_python/cert_code.py
@@ -53,6 +53,6 @@ def cert(h_name):
 
     with open('private.key', 'wb') as c:
         c.write(my_key_pem)
-        result = { my_cert_pem, my_key_pem }
+        result = {"my_cert_pem": str(my_cert_pem), "my_key_pem": str(my_key_pem) }
         print(json.dumps(result))
         return json.dumps(result)
